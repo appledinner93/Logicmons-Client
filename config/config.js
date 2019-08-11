dc10a6cd21f228cda5db6b594576bfeebcf5f4e3
ADinnerofSpaghetti <peterswanton191@bellsouth.net>
2019-08-11 13:58:14 -0400

    oof

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 var Config = Config || {};
 
-/* version */ Config.version = "0.11.2 (af3e2f5e)";
+/* version */ Config.version = "0.11.2 (b845858d)";
 
 Config.bannedHosts = ['cool.jit.su', 'pokeball-nixonserver.rhcloud.com'];
 
